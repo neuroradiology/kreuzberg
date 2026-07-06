c27e08f99312d8806f86dff071bfb74a49489914
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-07-06 13:57:10 +0200

    chore: regenerate bindings on rc.11 base
    Rebased feat/document-counts-and-split onto v1.0.0-rc.11; regenerate all
    Alef-managed bindings/e2e/docs so DocumentCounts propagates on the new base.

--- packages/swift/Sources/RustBridge/xberg-swift.swift
+++ packages/swift/Sources/RustBridge/xberg-swift.swift
@@ -27537,3 +27537,6 @@ func __swift_bridge__SwiftRerankerBackendBox__free (ptr: UnsafeMutableRawPointer
 func __swift_bridge__SwiftTokenizerBackendBox__free (ptr: UnsafeMutableRawPointer) {
     let _ = Unmanaged<SwiftTokenizerBackendBox>.fromOpaque(ptr).takeRetainedValue()
 }
+
+
+
